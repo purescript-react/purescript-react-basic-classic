8c02ed1b7ea064f9304ae78e07e036770e597325
Matthew Russell <matthew.russell@narvar.com>
2022-07-08 11:25:03 -0700

    React classic with React 18

--- examples/controlled-input/src/Main.purs
+++ examples/controlled-input/src/Main.purs
@@ -6,7 +6,7 @@ import ControlledInput (controlledInput)
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Exception (throw)
-import React.Basic.DOM (render)
+import React.Basic.DOM.Client (createRoot, renderRoot)
 import Web.DOM.NonElementParentNode (getElementById)
 import Web.HTML (window)
 import Web.HTML.HTMLDocument (toNonElementParentNode)
@@ -17,6 +17,7 @@ main = do
   container <- getElementById "container" =<< (map toNonElementParentNode $ document =<< window)
   case container of
     Nothing -> throw "Container element not found."
-    Just c  ->
+    Just c  -> do
+      root <- createRoot c
       let app = controlledInput unit
-       in render app c
+      renderRoot root app
